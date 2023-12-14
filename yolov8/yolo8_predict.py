c93075ac87116a1e2103e2306af808a6f4852ad2
strakajk <jakubstraka123@gmail.com>
2023-12-14 14:07:31 +0100

    input specification

--- yolov8/yolo8_predict.py
+++ yolov8/yolo8_predict.py
@@ -1,38 +1,42 @@
 from ultralytics import YOLO
-from yolo8_modules import SplitDetectionPredictor, SplitDetectionModel
+from ultralytics.engine.results import Results
 import torch
+from torch import Tensor
+from torch.nn import Module
+from typing import Union, List
+from yolo8_modules import SplitDetectionPredictor, SplitDetectionModel
 
 
-def predict_head(model, im0s, save_layers, imgsz=640):
-    predictor = SplitDetectionPredictor(model, overrides={"imgsz": imgsz})
+def predict_head(model: Module, input_image: Union[str, Tensor], save_layers: List[int], image_size: int = 640) -> dict:
+    predictor = SplitDetectionPredictor(model, overrides={"imgsz": image_size})
 
     # Prepare data
-    predictor.setup_source(im0s)
+    predictor.setup_source(input_image)
     batch = next(iter(predictor.dataset))
     predictor.batch = batch
-    path, im0s, vid_cap, s = batch
+    path, input_image, _, _ = batch
 
     # Preprocess
-    im = predictor.preprocess(im0s)
+    preprocessed_image = predictor.preprocess(input_image)
 
     # Head predict
-    y_head = model.forward_head(im, save_layers)
-    y_head["img_shape"] = im.shape[2:]
-    y_head["orig_shape"] = im0s[0].shape[:2]
+    y_head = model.forward_head(preprocessed_image, save_layers)
+    y_head["img_shape"] = preprocessed_image.shape[2:]
+    y_head["orig_shape"] = input_image[0].shape[:2]
 
     return y_head
 
 
-def predict_tail(model, y_head, imgsz=640):
-    predictor = SplitDetectionPredictor(model, overrides={"imgsz": imgsz})
+def predict_tail(model: Module, y_head: dict, image_size: int = 640) -> Results:
+    predictor = SplitDetectionPredictor(model, overrides={"imgsz": image_size})
 
     # Tail predict
-    preds = model.forward_tail(y_head)
+    predictions = model.forward_tail(y_head)
 
     # Postprocess
-    results = predictor.postprocess(preds, y_head["img_shape"], y_head["orig_shape"])
+    yolo_results = predictor.postprocess(predictions, y_head["img_shape"], y_head["orig_shape"])[0]
 
-    return results
+    return yolo_results
 
 
 if __name__ == "__main__":
@@ -43,17 +47,19 @@ if __name__ == "__main__":
     }
     split_layer, save_layers = splits["a"]
 
-    image_path = "../assets/coco_train_2017_small/000000005802.jpg"
+    # image = "../assets/coco_train_2017_small/000000005802.jpg"
     image = torch.rand(1, 3, 640, 640)
 
+    # head prediction
     pretrained_model = YOLO("yolov8m.pt").model
     model_head = SplitDetectionModel(pretrained_model, split_layer=split_layer)
     model_head = model_head.eval()
-    head_output = predict_head(model_head, image_path, save_layers)
+    head_output = predict_head(model_head, image, save_layers)
 
+    # tail prediction
     pretrained_model = YOLO("yolov8m.pt").model
     model_tail = SplitDetectionModel(pretrained_model, split_layer=split_layer)
     model_tail = model_tail.eval()
     results = predict_tail(model_tail, head_output)
-    print(results[0].boxes)
+    print(results.boxes)
     print(f"head layers: {len(model_head.head)}\ntail layers: {len(model_tail.tail)}")
